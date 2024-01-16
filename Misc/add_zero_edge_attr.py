64a15545c9da0d9397e06217d9149ddcf5bc3a20
ondrej <ondrej.brichta@gmail.com>
2024-01-16 22:19:33 +0100

    update to pytorch 2.0

--- Misc/add_zero_edge_attr.py
+++ Misc/add_zero_edge_attr.py
@@ -16,13 +16,14 @@ class AddZeroEdgeAttr(BaseTransform):
         self.edge_attr_size = edge_attr_size
 
     def __call__(self, data: Data) -> Data:
-        if 'edge_attr' in data:
+        if data.edge_attr is not None:
             if self.edge_attr_size == 0 and data.edge_attr.shape[1] > 0:
                 return data
-            data['edge_attr'] = cat([torch.zeros((data.edge_index.shape[1], max(self.edge_attr_size,1 ))), data.edge_attr],
-                                    dim=1).long()
+            data['edge_attr'] = cat(
+                [torch.zeros((data.edge_index.shape[1], max(self.edge_attr_size, 1))), data.edge_attr],
+                dim=1).long()
         else:
-            data['edge_attr'] = torch.zeros((data.edge_index.shape[1], max(self.edge_attr_size,1 )))
+            data['edge_attr'] = torch.zeros((data.edge_index.shape[1], max(self.edge_attr_size, 1)))
 
         return data
 
@@ -54,13 +55,13 @@ class AddZeroNodeAttr(BaseTransform):
         self.node_attr_size = node_attr_size
 
     def __call__(self, data: Data) -> Data:
-        if 'x' in data.keys:
+        if data.x is not None:
             # only add attribute ib there are none
             if self.node_attr_size == 0 and data.x.shape[1] > 0:
                 return data
-            data['x'] = cat([torch.zeros((data.num_nodes, max(self.node_attr_size,1 ))), data.x], dim=1).long()
+            data['x'] = cat([torch.zeros((data.num_nodes, max(self.node_attr_size, 1))), data.x], dim=1).long()
         else:
-            data['x'] = torch.zeros((data.num_nodes, max(self.node_attr_size,1 )))
+            data['x'] = torch.zeros((data.num_nodes, max(self.node_attr_size, 1)))
         return data
 
     def __repr__(self) -> str:
